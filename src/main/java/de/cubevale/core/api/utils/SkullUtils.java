fb86c34f8766ec8f94654c470004bd7caa4adb05
Konrad <107440414+Bapty1@users.noreply.github.com>
2024-01-07 00:44:20 +0100

    added player skull methods

--- src/main/java/de/cubevale/core/api/utils/SkullUtils.java
+++ src/main/java/de/cubevale/core/api/utils/SkullUtils.java
@@ -2,8 +2,10 @@ package de.cubevale.core.api.utils;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.destroystokyo.paper.profile.ProfileProperty;
+import de.cubevale.core.api.user.User;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
@@ -25,5 +27,19 @@ public class SkullUtils {
         itemStack.setItemMeta(skullMeta);
 
         return itemStack;
+    }
+
+    public static ItemStack getPlayerSkull(Player player) {
+        ItemStack itemStack = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
+
+        skullMeta.setOwningPlayer(player);
+        itemStack.setItemMeta(skullMeta);
+
+        return itemStack;
+    }
+
+    public static ItemStack getUserSkull(User user) {
+        return getPlayerSkull(Bukkit.getPlayer(user.getUuid()));
     }
 }
