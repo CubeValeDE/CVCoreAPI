8b4dbcc3e0c2e55d70b6035deada9ac84b296de7
Konrad <107440414+Bapty1@users.noreply.github.com>
2024-01-09 21:14:56 +0100

    Added gui menu

---
+++ src/main/java/de/cubevale/core/api/utils/gui/MenuHolder.java
@@ -0,0 +1,35 @@
+package de.cubevale.core.api.utils.gui;
+
+import org.bukkit.inventory.Inventory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class MenuHolder {
+
+    private final Inventory inventory;
+    private final Map<Integer, MenuButton> buttons;
+
+    public MenuHolder(Inventory inventory) {
+        this.inventory = inventory;
+        this.buttons = new HashMap<>();
+    }
+
+    public void setButton(int slot, MenuButton button) {
+        this.buttons.put(slot, button);
+    }
+
+    public MenuButton getButton(int slot) {
+        if (slot < 0 || slot >= buttons.size()) return null;
+
+        return buttons.get(slot);
+    }
+
+    public void unsetButton(int slot) {
+        this.buttons.remove(slot);
+    }
+
+    public void clearButtons() {
+        this.buttons.clear();
+    }
+}
