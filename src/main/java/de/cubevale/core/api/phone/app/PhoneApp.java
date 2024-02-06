ba19df078d1f0a326ea2806892c5d6ee05d018f6
Baptist <107440414+Bapty1@users.noreply.github.com>
2024-02-06 15:05:20 +0100

    added PhoneAppOptions, on execute and app icon

--- src/main/java/de/cubevale/core/api/phone/app/PhoneApp.java
+++ src/main/java/de/cubevale/core/api/phone/app/PhoneApp.java
@@ -1,4 +1,24 @@
 package de.cubevale.core.api.phone.app;
 
-public interface PhoneApp {
+import de.cubevale.core.api.user.User;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
+
+public abstract class PhoneApp {
+
+    private final PhoneAppOptions phoneAppOptions;
+
+    public PhoneApp() {
+        this.phoneAppOptions = Objects.requireNonNull(
+                getClass().getAnnotation(PhoneAppOptions.class), "PhoneAppOptions annotation is missing!");
+    }
+
+    public abstract void onExecute(User user);
+
+    public abstract ItemStack getAppIcon();
+
+    public PhoneAppOptions getPhoneAppOptions() {
+        return phoneAppOptions;
+    }
 }
